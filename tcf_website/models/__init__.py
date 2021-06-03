76b5395d080a9f0cb96e90c2e8aed6cf634285b5
nmahajan <n4mahajan@gmail.com>
2021-06-03 01:45:04 -0400

    Votes no longer broken for review but still not working for reply
    Issues with the replyvote model for some reason; can't seem to find the new model

--- tcf_website/models/__init__.py
+++ tcf_website/models/__init__.py
@@ -5,4 +5,4 @@
 # See
 # https://docs.djangoproject.com/en/3.0/topics/db/models/#organizing-models-in-a-package
 
-from .models import School, Department, Subdepartment, User, Instructor, Semester, Course, Section, Review, Reply, Vote, CourseGrade, CourseInstructorGrade
+from .models import School, Department, Subdepartment, User, Instructor, Semester, Course, Section, Review, Reply, Vote, ReplyVote, CourseGrade, CourseInstructorGrade
